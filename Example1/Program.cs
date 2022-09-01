24fb3844b72229a41467c64d242d0d0501243481
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-01 20:57:17 +0700

    Задача1

---
+++ Example1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input number 1: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Input number 2: ");
+int B = int.Parse(Console.ReadLine());
+
+if (A > B)
+{
+    Console.Write("A");
+}
+else
+{
+    Console.Write("B");
+}
