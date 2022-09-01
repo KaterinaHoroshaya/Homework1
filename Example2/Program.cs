a0edf1358dd7152251b246e71609e4fbd64baaad
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-01 21:20:53 +0700

    Задача2

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿Console.WriteLine("Input number 1: ");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Input number 2: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine("Input number 3: ");
+int C = int.Parse(Console.ReadLine());
+
+int max = A;
+
+if (A > max)
+{
+    max = A;
+}
+else if (B > max)
+{
+    max = B;
+}
+else if (C > max)
+{
+    max = C;
+}
+Console.WriteLine(max);
