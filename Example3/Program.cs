3eb837c412cf5a758fc4f8185af1644308e1405d
Katerina <m-a-c-u-k-a@mail.ru>
2022-09-01 22:18:57 +0700

    Задача3

---
+++ Example3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Input number: ");
+int i = int.Parse (Console.ReadLine());
+if (i%2==0)
+{
+    Console.Write("Ваше число чётное!");
+}
+else 
+{
+    Console.Write("Ваше число нечётное!");
+}
